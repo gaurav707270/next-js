56baeb6aa5ef6b767536bf47fb47a58e46df2061
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-18 10:21:03 +0530

    Update page.js

--- task-manager/app/projects/create/page.js
+++ task-manager/app/projects/create/page.js
@@ -3,7 +3,7 @@
 // import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import ProjectForm from "../../components/ProjectForm";
-// import { createProject } from "../../../services/projectApi";
+import { createProject } from "../../../services/projectApi";
 
 export default function CreateProjectPage() {
     // const router = useRouter();
