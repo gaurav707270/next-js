bab92da6f227b2139cd74f319cf9158b41a823f9
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-08-18 10:21:11 +0530

    Fix import statement for React in page.js

--- task-manager/app/projects/create/page.js
+++ task-manager/app/projects/create/page.js
@@ -1,6 +1,6 @@
 "use client";
 
-// import React, { useState } from "react";
+import React, { useState } from "react";
 import { useRouter } from "next/navigation";
 import ProjectForm from "../../components/ProjectForm";
 import { createProject } from "../../../services/projectApi";
