e491faccc02a5b9772d4bed06272e860799a54f5
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-07 23:11:02 +0530

    Update DataTable.js

--- user-app-data-table/app/components/DataTable.js
+++ user-app-data-table/app/components/DataTable.js
@@ -6,7 +6,7 @@ import SearchBar from "./SearchBar";
 import UserTable from "./UserTable";
 import UserForm from "./UserForm";
 import Pagination from "./Pagination";
-// import UserViewModal from "./UserViewModal";
+import UserViewModal from "./UserViewModal";
 
 // const API_URL = "http://localhost:3001/users";
 
