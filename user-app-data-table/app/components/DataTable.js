7be2db3f0f37078e38e9a35eb33321ea289b80bf
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-07 23:09:22 +0530

    Update DataTable.js

--- user-app-data-table/app/components/DataTable.js
+++ user-app-data-table/app/components/DataTable.js
@@ -4,7 +4,7 @@
 
 import SearchBar from "./SearchBar";
 import UserTable from "./UserTable";
-import UserForm from "./UserForm";
+// import UserForm from "./UserForm";
 import Pagination from "./Pagination";
 import UserViewModal from "./UserViewModal";
 
