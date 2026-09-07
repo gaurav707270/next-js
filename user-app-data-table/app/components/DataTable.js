d5b863a693e8bd40a08b98bb5ac4c1305bda8172
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-07 23:11:32 +0530

    Update DataTable.js

--- user-app-data-table/app/components/DataTable.js
+++ user-app-data-table/app/components/DataTable.js
@@ -15,7 +15,7 @@ const ITEMS_PER_PAGE = 5;
 
 export default function DataTable() {
 
-    // const [users, setUsers] = useState([]);
+    const [users, setUsers] = useState([]);
 
     const [loading, setLoading] = useState(true);
 
