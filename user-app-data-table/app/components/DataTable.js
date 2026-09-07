e36b52960d8232e4f73f66a0f2d2a61defd732d1
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-07 23:10:45 +0530

    Update DataTable.js

--- user-app-data-table/app/components/DataTable.js
+++ user-app-data-table/app/components/DataTable.js
@@ -1,6 +1,6 @@
 "use client";
 
-// import { useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 
 import SearchBar from "./SearchBar";
 // import UserTable from "./UserTable";
