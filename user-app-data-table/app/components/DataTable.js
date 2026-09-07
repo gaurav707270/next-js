2f786029700c969642e21f24b2650537bae307d7
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-07 23:11:27 +0530

    Update DataTable.js

--- user-app-data-table/app/components/DataTable.js
+++ user-app-data-table/app/components/DataTable.js
@@ -13,7 +13,7 @@ const API_URL = "http://localhost:3001/users";
 const ITEMS_PER_PAGE = 5;
 
 
-// export default function DataTable() {
+export default function DataTable() {
 
     // const [users, setUsers] = useState([]);
 
